187a9c9f52a8972758df935bbb6c47e53945096c
DominikPieprzyk <61658908+DominikPieprzyk@users.noreply.github.com>
2022-02-21 19:44:46 +0100

    Changes

--- Program.cs
+++ Program.cs
@@ -6,14 +6,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Data;
+using LibApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApplication1
+namespace LibApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<ApplicationDbContext>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                context.Database.Migrate();
+                SeedData.Initialize(services);
+                logger.LogInformation("Database initialized");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error while migrating data");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,10 @@
+using LibApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,8 +12,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Repositories;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace WebApplication1
+namespace LibApp
 {
     public class Startup
     {
@@ -23,6 +29,26 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                    options.EnableSensitiveDataLogging();
+                });
+
+
+            services.AddScoped<IBookRepository, BookRepository>();
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager<SignInManager<IdentityUser>>();
+                //.AddRoleManager<RoleManager<IdentityRole>>();
+
             services.AddControllersWithViews();
         }
 
@@ -32,6 +58,7 @@ namespace WebApplication1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
@@ -44,18 +71,15 @@ namespace WebApplication1
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "BooksByReleaseDate",
-                    pattern: "books/released/{year}/{month}",
-                    defaults: new { controller = "Books", action = "ByReleaseDate"}
-                );
-                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
